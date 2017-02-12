f0ae665ada932ac996d54374885540a2f63bc118
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-12 18:54:29 +0200

    Initial structure for app

---
+++ src/app/account/mainview/mainview.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: "my-account-mainview",
+  templateUrl: "mainview.component.html",
+  styleUrls: [ "mainview.component.css"]
+})
+export class MainviewComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import {MainviewComponent} from "./account/mainview/mainview.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainviewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
