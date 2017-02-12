5181d5a0e66bf98448df2632b99d987835ab106d
Kalle Launiala <kalle.launiala@protonit.net>
2017-02-12 17:57:37 +0200

    Updated Typescript to handle async/await

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  async testingCompiler() {
+    await this.callee();
+  }
+
+  async callee() {
+
+  }
 }
